4752290da504edab62e83cbdffd8d6f41cd60bd0
phuong.tran <phuong.tran@mesoneer.io>
2022-09-26 23:17:40 +0700

    Update source code with latest version of retry

--- JavaCoding/src/main/java/multithread/nonblocking/HandleExeptionAndRetryWithoutSchedule.java
+++ JavaCoding/src/main/java/multithread/nonblocking/HandleExeptionAndRetryWithoutSchedule.java
@@ -1,28 +1,38 @@
 package multithread.nonblocking;
 
 import java.time.Duration;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class HandleExeptionAndRetry {
-    private static final Duration WAIT_BETWEEN = Duration.ofMillis(20);
+// Version 2 uses the CompletableFuture without supplier. The result will work as expected, but it
+// is the foundation for the implementation of HandleExeptionAndRetryLatest
+public class HandleExeptionAndRetryWithoutSchedule {
+    private static final Duration WAIT_BETWEEN = Duration.ofMillis(2000);
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         TestExceptionService exception = new TestExceptionService();
         // Best way to handle run async in another thread and retry
         // no exception: run only once
         // exception: run 3 times
-        int age = -1;
+        AtomicInteger age = new AtomicInteger(-1);
         CompletableFuture<Void> attempter = CompletableFuture.runAsync(() -> {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-            System.out.println("executing my custom action here..." + Thread.currentThread().getName() + "...");
-            if (age < 0) {
+            System.err.println("executing my custom action here..." + Thread.currentThread().getName() + " Time: " + System.currentTimeMillis());
+//            age.set(new Random().nextInt(2));
+            System.out.println("Age: " + age);
+            if (age.get() < 0) {
                 throw new IllegalArgumentException("Age can not be negative");
             }
+        });
+
+        attempter.thenRun(() -> {
+            System.out.println("Then run to trigger notification: " + Thread.currentThread().getName());
         });
         Retries.withRetries(attempter, t -> true, 3, WAIT_BETWEEN);
 
@@ -30,6 +40,8 @@ public class HandleExeptionAndRetry {
         // If you don't wait for the action to complete, the main thread will be finished and no output will be written to console.'
         Thread.sleep(9000);
         System.out.println("Finished");
+
+        Retries.SCHEDULER.shutdown();
 
 //        OUTPUT:
 //        executing my custom action here...ForkJoinPool.commonPool-worker-1...
--- JavaCoding/src/main/java/multithread/nonblocking/Retries.java
+++ JavaCoding/src/main/java/multithread/nonblocking/Retries.java
@@ -1,6 +1,7 @@
 package multithread.nonblocking;
 
 import java.time.Duration;
+import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -11,14 +12,14 @@ import java.util.function.Predicate;
 
 public class Retries {
 
-    private static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
+    public static final ScheduledExecutorService SCHEDULER = Executors.newScheduledThreadPool(1);
 
     public static <T> void withRetries(CompletableFuture<T> attempter,
             Predicate<Throwable> shouldRetry,
             int attempts, Duration waitBetween) {
         // retries run via the scheduler
+        // The first time no need to wait so you can set the wait between to 0ms
         Executor scheduler = runnable -> SCHEDULER.schedule(runnable, waitBetween.toMillis(), TimeUnit.MILLISECONDS);
-
 
         flatten(attempter.thenApply((value) -> {
                     System.out.println("Run the fist logic here..." + String.valueOf(1) + " - " + System.currentTimeMillis());
@@ -29,18 +30,25 @@ public class Retries {
                     }
                     return CompletableFuture.completedFuture(value);
                 })
-                .exceptionally(throwable -> retry(attempter, 1, throwable, shouldRetry, attempts, scheduler)));
+                .handleAsync((res, ex) -> {
+                    if (ex != null) {
+                        System.out.println("Oops! We have an exception - " + ex.getMessage());
+                        retry(attempter, 1, ex.getMessage(), shouldRetry, attempts, scheduler);
+                        return null;
+                    }
+                    return res;
+                }, scheduler));
     }
 
     private static <T> CompletableFuture<T> retry(CompletableFuture<T> attempter,
             int attemptsSoFar,
-            Throwable throwable,
+            String throwable,
             Predicate<Throwable> shouldRetry,
             int maxAttempts,
             Executor scheduler) {
         System.out.println(
                 "Attempting to retry: " + attemptsSoFar + " maxAttempts " + maxAttempts + " Thread: " + Thread.currentThread()
-                        .getName());
+                        .getName() + " Time: " + System.currentTimeMillis());
         // cannot retry if we're at the max attempts or the predicate doesn't like the error
         int nextAttempt = attemptsSoFar + 1;
         if (nextAttempt > maxAttempts) {
@@ -49,9 +57,12 @@ public class Retries {
             return null;
         }
 
+//        attempter = (CompletableFuture<T>) getFile();
+
         return flatten(attempter
-                .thenApply((value) -> {
-                    System.out.println("Run the fist logic here..." + String.valueOf(attemptsSoFar) + " - " + System.currentTimeMillis());
+                .thenApplyAsync((value) -> {
+                    System.out.println("Run the second logic here..." + String.valueOf(
+                            attemptsSoFar) + " - " + System.currentTimeMillis());
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
@@ -59,11 +70,34 @@ public class Retries {
                     }
                     return CompletableFuture.completedFuture(value);
                 })
-                .exceptionally(nextThrowable ->
-                        retry(attempter, nextAttempt, nextThrowable, shouldRetry, maxAttempts, scheduler)));
+                .handleAsync((res, ex) -> {
+                            if (ex != null) {
+                                System.out.println("Oops! We have an exception - " + ex.getMessage());
+                                retry(attempter, nextAttempt, ex.getMessage(), shouldRetry, maxAttempts, scheduler);
+                                return null;
+                            }
+                            return res;
+                        }
+                        , scheduler));
     }
 
     private static <T> CompletableFuture<T> flatten(CompletableFuture<CompletableFuture<T>> completableCompletable) {
         return completableCompletable.thenCompose(Function.identity());
+    }
+
+    public static CompletableFuture<Void> getFile() {
+        CompletableFuture<Void> attempter = CompletableFuture.runAsync(() -> {
+            int age = 0;
+            System.out.println("executing my custom action here..." + Thread.currentThread().getName() + " Time: " + System.currentTimeMillis());
+//            age = new Random().nextInt(2);
+            System.out.println("Age: " + age);
+            if (age == 0) {
+                throw new IllegalArgumentException("Age can not be negative");
+            }
+        });
+        attempter.thenRun(() -> {
+            System.out.println("Notification");
+        });
+        return attempter;
     }
 }
