df5e9b8d3d7be9e4d4451fff0d114cce4992b41d
phuong.tran <phuong.tran@mesoneer.io>
2022-10-04 22:19:46 +0700

    Update source code

---
+++ JavaCoding/src/main/java/collection/CheckCollectionIsEmpty.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+//https://stackoverflow.com/questions/12721076/best-practice-to-validate-null-and-empty-collection-in-java
+public class CheckCollectionIsEmpty {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<String>();
+//        Add this dependency to pom.xml file
+//        <dependency>
+//			<groupId>org.apache.commons</groupId>
+//			<artifactId>commons-pool2</artifactId>
+//		</dependency>
+
+//        if (CollectionUtils.isEmpty(list)) {
+
+//        }
+    }
+}
