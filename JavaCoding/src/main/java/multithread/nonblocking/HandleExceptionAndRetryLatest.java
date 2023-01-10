f1d4f3b7d9823081d85b0ce9baae58783744a30a
phuong.tran <tranduyphuong20100@gmail.com>
2023-01-10 14:13:03 +0700

    Update source code

--- JavaCoding/src/main/java/multithread/nonblocking/HandleExceptionAndRetryLatest.java
+++ JavaCoding/src/main/java/multithread/nonblocking/HandleExceptionAndRetryLatest.java
@@ -93,3 +93,41 @@ public class HandleExceptionAndRetryLatest {
 //        Finished retrying: exceeded max attempts: pool-1-thread-1
     }
 }
+
+
+//OUTPUT 2:
+//        First retry attempt: main
+//        --- Start first attempt: 1673334640604
+//        => Download file: Start: 1673334640609 at Thread ForkJoinPool.commonPool-worker-1
+//        => Download file: Executing my custom action here...ForkJoinPool.commonPool-worker-1...
+//        --- End first attempt: 1673334640610
+//        ------ Thread: main i: 0 Time:1673334641618
+//        => Download file: End: 1673334642614 at Thread ForkJoinPool.commonPool-worker-1
+//        ------ Thread: main i: 1 Time:1673334642622
+//        ------ Thread: main i: 2 Time:1673334643627
+//        Oops! We have an exception - java.lang.IllegalArgumentException: Age can not be negative
+//        Attempting to retry: 1 maxAttempts 3 Thread: pool-1-thread-1 Time: 1673334644627
+//        ------ Thread: main i: 3 Time:1673334644633
+//        ------ Thread: main i: 4 Time:1673334645639
+//        => Download file: Start: 1673334646634 at Thread ForkJoinPool.commonPool-worker-1
+//        => Download file: Executing my custom action here...ForkJoinPool.commonPool-worker-1...
+//        ------ Thread: main i: 5 Time:1673334646640
+//        ------ Thread: main i: 6 Time:1673334647642
+//        => Download file: End: 1673334648641 at Thread ForkJoinPool.commonPool-worker-1
+//        ------ Thread: main i: 7 Time:1673334648647
+//        ------ Thread: main i: 8 Time:1673334649649
+//        Oops! We have an exception - java.lang.IllegalArgumentException: Age can not be negative
+//        Attempting to retry: 2 maxAttempts 3 Thread: pool-1-thread-1 Time: 1673334650645
+//        ------ Thread: main i: 9 Time:1673334650655
+//        ------ Thread: main i: 10 Time:1673334651661
+//        => Download file: Start: 1673334652649 at Thread ForkJoinPool.commonPool-worker-1
+//        => Download file: Executing my custom action here...ForkJoinPool.commonPool-worker-1...
+//        ------ Thread: main i: 11 Time:1673334652663
+//        ------ Thread: main i: 12 Time:1673334653665
+//        => Download file: End: 1673334654655 at Thread ForkJoinPool.commonPool-worker-1
+//        ------ Thread: main i: 13 Time:1673334654670
+//        ------ Thread: main i: 14 Time:1673334655674
+//        Oops! We have an exception - java.lang.IllegalArgumentException: Age can not be negative
+//        Attempting to retry: 3 maxAttempts 3 Thread: pool-1-thread-1 Time: 1673334656661
+//        Finished retrying: exceeded max attempts: pool-1-thread-1
+//        Finished main...
